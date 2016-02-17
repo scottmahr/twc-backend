03fac51386d7ca7edebabd41ed3e9745a458415a
Scott Mahr <Scott Mahr>
2016-02-16 16:49:25 -0800

    added way to remove drop ins

--- public/js/controllers/AdminCtrl.js
+++ public/js/controllers/AdminCtrl.js
@@ -1,9 +1,14 @@
 var app = angular.module("twc");
 
-app.controller('AdminCtrl', ['$scope','State', 
-      function($scope,State){
+app.controller('AdminCtrl', ['$scope','State','DropIns', 
+      function($scope,State,DropIns){
     $scope.s = State;
  
+    $scope.deleteDropIn = function(dropIn){
+        console.log('deleting... '+dropIn._id)
+
+        DropIns.deleteDropIn(dropIn);
+    }
    
 
 }]);
